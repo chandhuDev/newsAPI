becbd689a926e5769b3ecbf7b8f008193e50cc64
chandhu <chandhu.mpc19@gmail.com>
2022-09-29 01:29:37 +0530

    removed logs for debugging and added comments for the routes

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-
-const express=require("express")
 require("dotenv").config()
+const express=require("express") //importing express framework
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-const NewsAPI = require('newsapi');
-const apiValue=process.env.API
-const newsapi = new NewsAPI(apiValue);
-app.use("/topNewsInIndia",async(req,res)=>{
-    try{
-const newsDetails=await newsapi.v2.topHeadlines({
-    
-    
-    pageSize:5,
-    country:'in',
-  })
-  res.status(200).json({News:newsDetails})
-  console.log(newsDetails)
-}
+const NewsAPI = require('newsapi')//importing the newsapi package
 
+const apiValue=process.env.API // geting the value of my own API from .env file
+const newsapi = new NewsAPI(apiValue) 
+
+//Routes for newsAPI
+//It is the route for getting the top trending news in India by passing optional parameters 
+//pageSize and country of news we are going to fetch from this route
+app.use("/topNewsInIndia",async(req,res)=>{
+try{
+        const newsDetails=await newsapi.v2.topHeadlines({
+        pageSize:5,
+        country:'in',
+         })
+      res.status(200).json({News:newsDetails})
+    }
 catch(e){
     console.log("error in route")
     console.log(e.message)
 }
 })
 
-
+//It is the route for listing down of different types of news
+//like for Eg: business, entertainment , technology
 app.use("/listOfNewsTypes",async(req,res)=>{
 try{
     const {type}=req.query
-const newsDetails=await newsapi.v2.sources({
-    
-    
+    const newsDetails=await newsapi.v2.sources({
     language: 'en',
     category:`${type}`,
     country:'us',
-    
-    
-  })
+    })
   res.status(200).json({listOfNewsTypes:newsDetails})
-}
+  }
 catch(e){
     console.log("error in list filter")
     console.log(e.message)
@@ -47,31 +44,27 @@ catch(e){
 })
 
 
-
+//It is the route for query filters for the news by passing optional parameters
+//like for Eg: query q i.e type , from and to Date , sortBy and pageSize that 
+//is number of results we are trying to fetch
 app.use("/searchNews",async(req,res)=>{
 try{
     const {type,from,to,sortBy}=req.query
-   const newsDetails=await newsapi.v2.everything({
+    const newsDetails=await newsapi.v2.everything({
         q: `${type}`,
-        
         from: `${from}`,
         to: `${to}`,
         language: `en`,
         sortBy: `${sortBy}`,
         pageSize:`5`,
       })
-res.status(200).json({searchNews:newsDetails})
+    res.status(200).json({searchNews:newsDetails})
 }
 catch(e){
     console.log("error in search filter")
     console.log(e.message)
-    
-}
+    }
 })
 
-
-
-
-
-
+//exporting app instance to index.js file to listen the server
 module.exports=app
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const app=require("./app")
 require("dotenv").config()
-const PORT=process.env.PORT || 6500
+const app=require("./app")
+const PORT=process.env.PORT || 6500 //getting PORT value from .env value and giving default value as it is hosted in heroku it requires default value
+//servers starts listening at defined PORT number and logging it to check is it working or not
 app.listen(PORT,()=>{
     console.log(`server is succesfully listening at ${PORT}`)
 })
